3a37682388cff3b64b4c2b9b5a875b337d432967
Bonginkosi Mahlangu <BMahlangu@jhb.dvt.co.za>
2018-06-29 13:04:56 +0200

    Added Git interface with 3 methods

---
+++ GroupGit/Models/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupGit.Models
+{
+    interface IRepository
+    {
+        void HelloGit();
+        int GetGitID();
+
+        string GetGitRepository();
+    }
+}
